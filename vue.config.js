2c8b6fd43c83a88d4eda77ca26bcd3d10a56f5b2
Ethan <me@ethan.link>
2020-04-03 23:10:13 +1100

    Tweaked PWA options

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   pwa: {
+    name: "COVID-19 data for New South Wales",
+    manifestOptions: {
+      short_name: "COVID-19 Data"
+    },
     workboxOptions: {
       exclude: [`_redirects`, `_headers`],
       // https://forum.vuejs.org/t/vue-cli-3-project-with-pwa-not-busting-cache-on-new-builds/50252/7
