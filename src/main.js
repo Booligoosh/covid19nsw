fd859ddbf8b8db326bc24aae2f23de894db1c702
Ethan <me@ethan.link>
2020-04-03 23:26:17 +1100

    Google Analytics

--- src/main.js
+++ src/main.js
@@ -9,6 +9,15 @@ Vue.config.productionTip = false;
 import Chart from "vue2-frappe";
 Vue.use(Chart);
 
+import VueGtag from "vue-gtag";
+Vue.use(
+  VueGtag,
+  {
+    config: { id: "UA-103555680-12" }
+  },
+  router
+);
+
 new Vue({
   store,
   router,
