1c963c565f9674e9a17e5fd6670af6e885bb145b
Ethan <me@ethan.link>
2020-04-04 18:39:13 +1100

    Tweaked description text

--- src/components/PostcodePageMetadataChanger.vue
+++ src/components/PostcodePageMetadataChanger.vue
@@ -10,7 +10,7 @@ export default {
       "D MMMM YYYY"
     )}, there are ${this.totalCases} cases of COVID-19 in the postcode ${
       this.$route.params.postcode
-    }. Click to see how this number has changed over time, as well as new cases per day.`;
+    }. Click to see the latest data for your postcode, and view graphs of how it has changed over time.`;
   }
 };
 </script>
