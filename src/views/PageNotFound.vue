22a6a17182de8288b1ddd0bffb14e59944877f45
Ethan <me@ethan.link>
2020-04-04 21:35:57 +1100

    Added 404 page

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-not-found">
+    <h1>Page not found.</h1>
+    <router-link to="/">Back to home</router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.page-not-found {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
