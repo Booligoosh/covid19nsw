94a991e343f1e9afe3b855cc45d6325becf8c4ed
Ethan <me@ethan.link>
2020-04-08 15:32:39 +1000

    Don't precache sourcemaps

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
       short_name: "COVID-19 Data"
     },
     workboxOptions: {
-      exclude: [`_redirects`, `_headers`],
+      exclude: ["_redirects", "_headers", /.map$/],
       // https://forum.vuejs.org/t/vue-cli-3-project-with-pwa-not-busting-cache-on-new-builds/50252/7
       skipWaiting: true,
       clientsClaim: true
