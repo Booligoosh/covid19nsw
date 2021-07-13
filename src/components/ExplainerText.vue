4dd079b834db63802932cd350953e36183df17bb
Ethan <me@ethan.link>
2021-07-13 11:46:22 +1000

    Tweaked explainer text

--- src/components/ExplainerText.vue
+++ src/components/ExplainerText.vue
@@ -1,17 +1,17 @@
 <template>
   <div :class="{ 'is-above-data': isAboveData }">
     <p>
-      This site uses the
+      This site was built by
+      <a href="https://ethan.link" target="_blank">Ethan</a> and is not
+      affiliated with the NSW government.
+    </p>
+    <p>
+      It uses the
       <a href="https://data.nsw.gov.au/nsw-covid-19-data" target="_blank"
         >official data</a
       >
       provided by NSW Health, which is fetched fresh every time you load this
       page.
-    </p>
-    <p>
-      This site was built by
-      <a href="https://ethan.link" target="_blank">Ethan</a> and is not
-      affiliated with the NSW government.
     </p>
   </div>
 </template>
