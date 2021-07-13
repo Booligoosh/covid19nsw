1a5df0274acbf777db3e757cf69f5c346a8469b3
Ethan <me@ethan.link>
2021-07-13 22:24:28 +1000

    Improved About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="about-page">
-    <a href="#" @click.prevent="$router.go(-1)" class="about-page-back">
-      ← Back
-    </a>
-    <p>
-      This site was built by
-      <a href="https://ethan.link" target="_blank">Ethan</a> and is not
-      affiliated with the NSW government.
-    </p>
-    <p>
-      It uses the
-      <a href="https://data.nsw.gov.au/nsw-covid-19-data" target="_blank"
-        >official data</a
-      >
-      provided by NSW Health, which is fetched fresh every time you load this
-      page.
-    </p>
+    <div>
+      <h1>About this site</h1>
+      <p>
+        This site was built by
+        <a href="https://ethan.link" target="_blank">Ethan</a> and is not
+        affiliated with the NSW government.
+      </p>
+      <p>
+        It uses the
+        <a href="https://data.nsw.gov.au/nsw-covid-19-data" target="_blank"
+          >official data</a
+        >
+        provided by NSW Health, which is fetched fresh every time you load this
+        page.
+      </p>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .about-page {
-  &-back {
-    color: inherit;
-    text-decoration: none;
-    font-weight: bold;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  h1 {
+    font-size: 1.5rem;
   }
 }
 </style>
