58887efabaccb3981fe630b69c2abbc6eb2f2b38
Ethan <me@ethan.link>
2021-07-25 09:45:40 +1000

    Add changefreq=daily to ListPages in sitemap

--- generateSitemap.js
+++ generateSitemap.js
@@ -8,8 +8,10 @@ const postcodeRanges = [
 let sitemap = "";
 sitemap += '<?xml version="1.0" encoding="UTF-8"?>';
 sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-sitemap += "<url><loc>https://covid19nsw.ethan.link/postcodes</loc></url>";
-sitemap += "<url><loc>https://covid19nsw.ethan.link/councils</loc></url>";
+sitemap +=
+  "<url><loc>https://covid19nsw.ethan.link/postcodes</loc><changefreq>daily</changefreq></url>";
+sitemap +=
+  "<url><loc>https://covid19nsw.ethan.link/councils</loc><changefreq>daily</changefreq></url>";
 sitemap += "<url><loc>https://covid19nsw.ethan.link/alerts</loc></url>";
 sitemap += "<url><loc>https://covid19nsw.ethan.link/about</loc></url>";
 
