21675cb37524e9b326e56b0e7820cc57ab2be376
Ethan <me@ethan.link>
2021-07-24 12:06:43 +1000

    Align numbers to top of cell in compact mode

--- src/views/ListPage.vue
+++ src/views/ListPage.vue
@@ -384,6 +384,13 @@ table {
     font-weight: 500;
   }
 
+  td.value-number {
+    @media screen and (max-width: $compact-breakpoint) {
+      // Align numbers to top of cell rather than middle
+      vertical-align: baseline;
+    }
+  }
+
   td.postcode-number,
   td.council-name {
     font-weight: bold;
