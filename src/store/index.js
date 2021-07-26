89e6bdc193652bc97d330f0af0f9fbfd5aa90af9
Ethan <me@ethan.link>
2021-07-26 17:16:00 +1000

    Store date of last visit so I can display update messages in future

--- src/store/index.js
+++ src/store/index.js
@@ -14,6 +14,9 @@ const CASES_URL = "/data/cases.json";
 const CASE_LOCATIONS_URL =
   "https://data.nsw.gov.au/data/dataset/0a52e6c1-bc0b-48af-8b45-d791a6d8e289/resource/f3a28eed-8c2a-437b-8ac1-2dab3cf760f9/download/venue-data-2020-dec-22-v3.json";
 
+// Store date of last visit so I can display update messages in future
+localStorage.lastVisit = new Date().toISOString();
+
 const store = new Vuex.Store({
   state: {
     cases: [],
