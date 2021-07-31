c36cf4764d7ed650400bc8613717b6e13cf7294d
Ethan <me@ethan.link>
2021-07-31 20:01:15 +1000

    Don't include inline app.css twice
    Based on https://github.com/DustinJackson/html-webpack-inline-source-plugin/issues/50#issuecomment-421896798

--- vue.config.js
+++ vue.config.js
@@ -24,9 +24,17 @@ module.exports = {
     ],
   },
   chainWebpack: (config) => {
+    // See https://github.com/DustinJackson/html-webpack-inline-source-plugin/issues/50#issuecomment-421896798
+    if (config.plugins.has("preload"))
+      config.plugin("preload").tap((args) => {
+        args[0].fileBlacklist.push(/app\..+?\.css$/);
+        return args;
+      });
+
     config
       .plugin("inline-source")
       .use(require("html-webpack-inline-source-plugin"));
+
     config.plugin("html").tap((args) => {
       args[0].inlineSource = "app\\..+?\\.css$";
       return args;
