0f79f6d5ed2fdbec2a2eb1642875bcc86c5d5f8f
Ethan <me@ethan.link>
2021-07-31 13:06:56 +1000

    Emergency SEO fix: Prerender /, /postcodes, /councils and /about

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
 module.exports = {
   pwa: {
     name: "COVID-19 NSW",
@@ -32,6 +35,24 @@ module.exports = {
   },
   css: {
     sourceMap: true,
+  },
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, "./dist"),
+        routes: ["/", "/postcodes", "/councils", "/about"],
+        postProcess(renderedRoute) {
+          // Ignore any redirects.
+          renderedRoute.route = renderedRoute.originalRoute;
+          // Remove <noscript> tag for prerendered pages as they work without JS
+          renderedRoute.html = renderedRoute.html.replace(
+            /<noscript>.+?<\/noscript>/g,
+            ""
+          );
+          return renderedRoute;
+        },
+      }),
+    ],
   },
   devServer: { port: 8081 },
 };
