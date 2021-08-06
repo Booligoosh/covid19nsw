7d61a692f541ef4294aacc4b90b21ca8d588c4e4
Ethan <me@ethan.link>
2021-08-06 16:08:30 +1000

    Fixed OUTBREAK_START_DATE lol

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const DEFAULT_PAGE_TITLE = "COVID-19 NSW";
 export const DEFAULT_PAGE_DESCRIPTION =
   "See the latest local COVID-19 information for your postcode/suburb/council in New South Wales.";
 export const ALL_TIME_START_DATE = "2020-01-25";
-export const OUTBREAK_START_DATE = "2021-06-17";
+export const OUTBREAK_START_DATE = "2021-06-16";
 export const OUTBREAK_START_DATE_FORMATTED = "16 Jun";
