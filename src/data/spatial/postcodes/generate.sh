e44eebf7a72e38bf91489cf71be60cf0b5351915
Ethan <me@ethan.link>
2021-08-10 13:03:02 +1000

    Reduce precision of geojson.json to 4dp

--- src/data/spatial/postcodes/generate.sh
+++ src/data/spatial/postcodes/generate.sh
@@ -1,2 +1,4 @@
 # Generates minified & transformed GeoJSON file ./geojson.json from ./source folder contents
-mapshaper ./source/POA_2016_AUST.shp -simplify dp 10% keep-shapes -o geojson.json format=geojson && node transformGeojson.js
+# 0.0001 = 4dp = ~11m precision
+# See https://dpstyles.tumblr.com/post/95952859425/how-does-the-precision-of-a-lat-long-change-with-the
+mapshaper ./source/POA_2016_AUST.shp -simplify dp 10% keep-shapes -o geojson.json format=geojson precision=0.0001 && node transformGeojson.js
