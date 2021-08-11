e412b0c73a74637025d7ad9ebf8bd7d54dcc2cb4
Ethan <me@ethan.link>
2021-08-11 15:02:50 +1000

    Fix the site not updating if it was last loaded with a service worker

--- public/yo.js
+++ public/yo.js
@@ -1,1 +1,10 @@
-// Will be overwritten by Netlify redirect in production.
+// This file is an override for users who last loaded this site when it still had the old service worker.
+// This script used to be loaded by that version, but is not by newer versions, so this is a way of injecting code into those older versions.
+
+const SEARCH_STRING = "?ref=sw_fix";
+
+if (window.location.search !== SEARCH_STRING) {
+  window.location.search = SEARCH_STRING;
+} else {
+  window.location.reload(true);
+}
