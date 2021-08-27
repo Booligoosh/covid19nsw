bcb6f201fb403e44f5e5abd45a48728d307e80e4
Ethan <me@ethan.link>
2021-08-27 13:36:29 +1000

    Add curl commands to generateSuburbsFiles.sh

--- src/data/generateSuburbsFiles.sh
+++ src/data/generateSuburbsFiles.sh
@@ -1,4 +1,6 @@
+curl https://www.nsw.gov.au/dcs_clinics_manager/suburbs > suburbs.json && \
 node generateSuburbListNSW.js && \
 node generateSuburbsForPostcode.js && \
 node generateLatLongForSuburbs.js && \
+curl https://raw.githubusercontent.com/matthewproctor/australianpostcodes/master/australian_postcodes.json > australian_postcodes.json && \
 node generateLatLongForPostcodes.js
