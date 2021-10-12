82aa1ea4f299b4368e0f1d2a33d60ceb65410007
Ethan <me@ethan.link>
2021-10-12 22:08:40 +1100

    Scroll to top of each page by default

--- src/router/index.js
+++ src/router/index.js
@@ -154,6 +154,17 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.hash) {
+      return {
+        selector: to.hash,
+      };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
 });
 
 let prevHistoryStateKey = null;
