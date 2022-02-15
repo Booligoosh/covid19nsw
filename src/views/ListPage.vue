0b5f608133ea307707253b08e1eaa7e97e3c4a92
Ethan <me@ethan.link>
2022-02-16 10:07:18 +1100

    Specify that data includes PCR + RAT

--- src/views/ListPage.vue
+++ src/views/ListPage.vue
@@ -46,9 +46,8 @@
       </div>
     </div>
     <div class="table-subtitle" v-else>
-      Last updated <mark>{{ lastUpdatedString }}</mark> by NSW Health, data up
-      to <mark>{{ temporalCoverageString }}</mark
-      >.
+      Updated <mark>{{ lastUpdatedString }}</mark> by NSW Health. Cases are from
+      both RAT and PCR tests.
       <div class="table-subtitle-disclaimer">
         *Many cases aren&rsquo;t added by NSW Health within 1 day, so
         today&rsquo;s numbers are less than final values.
